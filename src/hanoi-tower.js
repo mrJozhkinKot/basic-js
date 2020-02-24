a05c25fc445cb8459e2a385ae459dee18801b2a3
Yulia Shchuklina <scrambler88@mail.ru>
2020-02-24 23:09:44 +0300

    feat: add solution to hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,12 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    function calculateTurns (disksNumber) {
+        if (disksNumber == 1) {return 1;}
+    else { 
+      return  2 * calculateTurns(disksNumber-1) + 1;
+    } 
+    }
+    let turns = calculateTurns(disksNumber);
+    let seconds = turns / (turnsSpeed / 3600);
+    
+    return {turns: turns, seconds: seconds}
+    }
