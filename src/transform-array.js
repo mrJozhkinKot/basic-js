830c747684dc3da8e4f6ecc3d9a86e59d249874d
Yulia Shchuklina <scrambler88@mail.ru>
2020-02-25 22:37:59 +0300

    feat: add solution to transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,17 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {throw Error()}
+    let newArr = [];
+    for (i=0; i<arr.length; i++) {
+         if  (arr[i] == '--double-next') {
+          newArr.push(arr[i+1]);
+            }
+     else if (arr[i] == '--double-prev'){
+       newArr.push(arr[i-1]);
+      
+            }
+     else if (arr[i] == '--discard-prev') {newArr.pop();}
+     else if (arr[i] == '--discard-next') {arr.splice([i],1);}  
+     else {newArr.push(arr[i])}     
+          }   
+     return newArr.filter(function(i) { return i !== undefined});
+    };
