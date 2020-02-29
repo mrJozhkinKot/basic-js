ac520d9c781898587eff54cb344e9639774e01ec
Yulia Shchuklina <scrambler88@mail.ru>
2020-02-29 18:06:09 +0300

    add solution to repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,35 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+
+   
+    if (options.repeatTimes === undefined) options.repeatTimes = 1;
+    if (options.separator === undefined)
+     options.separator = '+';
+    if (options.additionRepeatTimes === undefined)
+     options.additionRepeatTimes = 1;
+    if (options.additionSeparator === undefined) 
+    options.additionSeparator = '|';
+    if (options.addition === undefined) 
+    options.addition = '';
+    
+    let res = '';
+   
+   
+    for (let elem=0; elem<options.repeatTimes; elem++) {
+      
+      res += str;
+    
+     for (let el=0; el<options.additionRepeatTimes; el++) {
+      res += options.addition;
+      if (el < options.additionRepeatTimes-1) {
+         res += options.additionSeparator;}
+     }
+       
+            
+       if (elem < options.repeatTimes-1) {
+         res += options.separator;
+       }
+       }
+       
+
+       return res;
+    };
