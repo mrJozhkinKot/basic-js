45f1e2cba44edc8e3567ed5e77fdae4e1538f9db
Yulia Shchuklina <scrambler88@mail.ru>
2020-03-01 21:17:16 +0300

    add solution to ciper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor (value){
+      this.value = value;
     }
+    
+    encrypt(message, key) {
+        if (message === undefined|| key === undefined) throw Error();
+      
+        let result = [];
+        let k=0;
+        
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].match(/[A-Za-z]/)) {                     
+           result.push(String.fromCharCode((message[i].toUpperCase().charCodeAt() -65 + key[k % key.length].toUpperCase().charCodeAt()-65)% 26 + 65));
+           
+           k++;
+            } else result.push(message[i]); 
+                 } 
+                return this.value == false? result.reverse().join(''):  result.join(''); 
+                          
+                }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
-}
+
+                
+
+    decrypt(message, key) {
+        if (message === undefined || key === undefined) throw Error();
+       
+        let result = [];
+        let k=0;
+    
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].match(/[A-Za-z]/)) {                     
+            result.push(String.fromCharCode(Math.abs(message[i].toUpperCase().charCodeAt() - key[k % key.length].toUpperCase().charCodeAt()+26)% 26 + 65));
+                       k++;
+            } else result.push(message[i]); 
+                  } 
+           
+                         
+                  return this.value == false? result.reverse().join(''):  result.join(''); 
+                }
+
+                 
+        }
+
 
 module.exports = VigenereCipheringMachine;
